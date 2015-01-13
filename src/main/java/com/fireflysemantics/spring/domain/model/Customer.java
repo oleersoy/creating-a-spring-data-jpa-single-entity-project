b9734d981c6bc46c62aced79f471a6b70bee31cb
Ole Ersoy <ole.ersoy@gmail.com>
2015-01-12 19:09:03 -0600

    Created the generating-a-schema-with-the-hibernate-4-maven-plugin branch

--- src/main/java/com/fireflysemantics/spring/domain/model/Customer.java
+++ src/main/java/com/fireflysemantics/spring/domain/model/Customer.java
@@ -5,7 +5,6 @@ import javax.persistence.Entity;
 import org.springframework.data.jpa.domain.AbstractPersistable;
 import static org.springframework.util.Assert.notNull;
 
-;
 
 @Entity
 public class Customer extends AbstractPersistable<Long> {
@@ -14,34 +13,36 @@ public class Customer extends AbstractPersistable<Long> {
 	 * serialVersionUID
 	 */
 	private static final long serialVersionUID = -5606205093782977644L;
-	private String name;
+	
+	private String firstName;
 	private String email;
 
+	/** Protected default constructor required by the JPA 2.0 Specification */
 	protected Customer() {
 	}
 
-	public Customer(String name, String email) {
-		notNull(name, "The customer name is null.");
-		notNull(name, "The email address is null.");
-		this.name = name;
+	public Customer(String firstName, String email) {
+		notNull(firstName, "The customer name is null.");
+		notNull(firstName, "The email address is null.");
+		this.firstName = firstName;
 		this.email = email;
 	}
 
 	@Override
 	public String toString() {
 		return String.format("Customer[id=%d, name='%s', email='%s']",
-				this.getId(), name, email);
+				this.getId(), firstName, email);
 	}
 
 	// ====================================================
 	// Getters and Setters
 	// ====================================================
-	public String getName() {
-		return name;
+	public String getFirstName() {
+		return firstName;
 	}
 
-	public void setName(String name) {
-		this.name = name;
+	public void setFirstName(String name) {
+		this.firstName = name;
 	}
 
 	public String getEmail() {
